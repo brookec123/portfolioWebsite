4d09f0269ced9962e12306850e9d82bb0b9e66f5
brookes123 <brookesynchro123@gmail.com>
2023-04-10 21:52:56 -0400

    added icons for skills

--- js/main.js
+++ js/main.js
@@ -60,17 +60,18 @@
     
     
     // Typed Initiate
-    if ($('.hero .hero-text h2').length == 1) {
-        var typed_strings = $('.hero .hero-text .typed-text').text();
-        var typed = new Typed('.hero .hero-text h2', {
+        if ($('.hero .hero-text h2').length == 1) {
+          var typed_strings = $('.hero .hero-text .typed-text').text();
+          var typed = new Typed('.hero .hero-text h2', {
             strings: typed_strings.split(', '),
             typeSpeed: 100,
             backSpeed: 20,
             smartBackspace: false,
             loop: true
-        });
-    }
-    
+          });
+        }
+      
+      
     
     // Skills
     $('.skills').waypoint(function () {
