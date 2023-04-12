9bd137326c1341a315196ac29773774fce0c70dc
brookes123 <brookesynchro123@gmail.com>
2023-04-11 21:13:31 -0400

    started to add first project

--- js/portfolio-script.js
+++ js/portfolio-script.js
@@ -116,7 +116,7 @@ function openPDF() {
     window.open(url, '_blank');
 }
 
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -124,16 +124,26 @@ function plusSlides(n) {
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }    
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
-  slides[slideIndex-1].style.display = "block";  
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
+
+// // Call the showSlides function on page load
+// window.onload = function() {
+//   showSlides(slideIndex);
+// };
+
+setInterval(function() {
+    showSlides(slideIndex);
+}, 1000);
