e80dd23cae2b8b18b7f30884dd1551faa8fa20e3
brookec123 <brookesynchro123@gmail.com>
2025-01-28 15:14:05 -0500

    updated content, fixed minor bugs with skill filter/search, and navbar styling and functionality

--- js/main.js
+++ js/main.js
@@ -1,178 +1,197 @@
 (function ($) {
-    "use strict";
+  "use strict";
 
-    // loader
-    var loader = function () {
-        setTimeout(function () {
-            if ($('#loader').length > 0) {
-                $('#loader').removeClass('show');
-            }
-        }, 1);
-    };
-    loader();
+  // loader
+  var loader = function () {
+    setTimeout(function () {
+      if ($("#loader").length > 0) {
+        $("#loader").removeClass("show");
+      }
+    }, 1);
+  };
+  loader();
 
-    // Initiate the wowjs
-    new WOW().init(); 
+  // Initiate the wowjs
+  new WOW().init();
 
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 200) {
-            $('.back-to-top').fadeIn('slow');
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
+
+  // Typed Initiate
+  if ($(".hero .hero-text h2").length == 1) {
+    var typed_strings = $(".hero .hero-text .typed-text").text();
+    var typed = new Typed(".hero .hero-text h2", {
+      strings: typed_strings.split(", "),
+      typeSpeed: 100,
+      backSpeed: 20,
+      smartBackspace: false,
+      loop: true,
+    });
+  }
+
+  // Testimonials carousel
+  $(".testimonials-carousel").owlCarousel({
+    center: true,
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+    },
+  });
+
+  // Global variables for search and filter
+  let skillsFilter = "*";
+  let searchQuery = "";
+
+  // Filter skills
+  $("#skills-flters li").on("click", function () {
+    $("#skills-flters li").removeClass("active");
+    $(this).addClass("active");
+    skillsFilter = $(this).data("filter");
+    applySearchAndFilter();
+  });
+
+  // Search skills
+  document
+    .getElementById("skillsSearch")
+    .addEventListener("input", function () {
+      searchQuery = this.value.toLowerCase();
+      applySearchAndFilter();
+    });
+
+  // Combined search and filter logic
+  function applySearchAndFilter() {
+    $(".skills-container").isotope({
+      filter: function () {
+        const skillName =
+          this.querySelector(".skill-name").textContent.toLowerCase();
+        const matchesSearch = skillName.includes(searchQuery);
+        const matchesFilter =
+          skillsFilter === "*" ||
+          $(this).hasClass(skillsFilter.replace(".", ""));
+        return matchesSearch && matchesFilter;
+      },
+    });
+  }
+
+  // Portfolio isotope and filter
+  var portfolioIsotope = $(".portfolio-container").isotope({
+    itemSelector: ".portfolio-item",
+    layoutMode: "fitRows",
+  });
+  $("#portfolio-flters li").on("click", function () {
+    $("#portfolio-flters li").removeClass("active");
+    $(this).addClass("active");
+
+    portfolioIsotope.isotope({ filter: $(this).data("filter") });
+  });
+
+  // Search Portfolio
+  document
+    .getElementById("portfolioSearch")
+    .addEventListener("input", function () {
+      const searchQuery = this.value.toLowerCase();
+      const projects = document.querySelectorAll(
+        ".portfolio-container .portfolio-item"
+      );
+
+      projects.forEach((project) => {
+        const projectName = project
+          .querySelector(".portfolio-text p")
+          .textContent.toLowerCase();
+        if (projectName.includes(searchQuery)) {
+          project.style.display = "block"; // Show matching projects
         } else {
-            $('.back-to-top').fadeOut('slow');
+          project.style.display = "none"; // Hide non-matching projects
         }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
-        return false;
+      });
     });
 
-    // Typed Initiate
-    if ($('.hero .hero-text h2').length == 1) {
-        var typed_strings = $('.hero .hero-text .typed-text').text();
-        var typed = new Typed('.hero .hero-text h2', {
-            strings: typed_strings.split(', '),
-            typeSpeed: 100,
-            backSpeed: 20,
-            smartBackspace: false,
-            loop: true
-        });
+  // Experience isotope and filter
+  var experienceIsotope = $(".timeline").isotope({
+    itemSelector: ".experience-container",
+    layoutMode: "vertical",
+    getSortData: {
+      category: "[class]",
+    },
+  });
+
+  $("#experience-flters li").on("click", function () {
+    $("#experience-flters li").removeClass("active");
+    $(this).addClass("active");
+
+    var filterValue = $(this).attr("data-filter");
+    experienceIsotope.isotope({ filter: filterValue });
+  });
+
+  document.addEventListener("DOMContentLoaded", () => {
+    var skillsIsotope = $(".skills-container").isotope({
+      itemSelector: ".skill-container",
+      layoutMode: "fitRows",
+    });
+
+    // Populate skills and reinitialize Isotope
+    fetch("data.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        populateSkills(data.skills);
+      })
+      .catch((error) => console.error("Error loading skills data:", error));
+  });
+
+  function populateSkills(skills) {
+    console.log("Populating skills...");
+    const skillsContainer = document.getElementById("skills-container");
+
+    if (!skillsContainer) {
+      console.error("Skills container not found!");
+      return;
     }
 
-    // Testimonials carousel
-    $(".testimonials-carousel").owlCarousel({
-        center: true,
-        autoplay: true,
-        dots: true,
-        loop: true,
-        responsive: {
-            0: {
-                items: 1
+    // Clear the container before adding new items
+    skillsContainer.innerHTML = "";
+
+    skills.forEach((skill) => {
+      const skillDiv = document.createElement("div");
+      skillDiv.className = `col-lg-3 col-md-6 skill-container ${skill.type}`;
+      skillDiv.style.margin = "20px";
+      skillDiv.innerHTML = `
+        <div class="skill-item" style="padding: 10px;">
+            ${
+              skill.icon.startsWith("fa") ||
+              skill.icon.startsWith("fab") ||
+              skill.icon.startsWith("fas")
+                ? `<i class="${skill.icon}"></i>`
+                : `<img src="${skill.icon}" alt="${skill.name} Logo">`
             }
-        }
-    });
-
-    // Skills isotope and filter
-    var skillsIsotope = $('.skills-container').isotope({
-        itemSelector: '.skill-container',
-        layoutMode: 'fitRows'
-    });
-
-    $('#skills-flters li').on('click', function () {
-        $("#skills-flters li").removeClass('active');
-        $(this).addClass('active');
-        skillsIsotope.isotope({ filter: $(this).data('filter') });
-    });
-
-    // Search Skills
-    document.getElementById('skillsSearch').addEventListener('input', function () {
-        const searchQuery = this.value.toLowerCase();
-        const skills = document.querySelectorAll('.skills-container .skill-container');
-
-        skills.forEach(skill => {
-            const skillName = skill.querySelector('.skill-name').textContent.toLowerCase();
-            if (skillName.includes(searchQuery)) {
-                skill.style.display = 'block';  // Show matching skills
-            } else {
-                skill.style.display = 'none';   // Hide non-matching skills
-            }
-        });
-        skillsIsotope.isotope('layout');  // Update Isotope layout
-    });
-
-    // Portfolio isotope and filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-    });
-    $('#portfolio-flters li').on('click', function () {
-        $("#portfolio-flters li").removeClass('active');
-        $(this).addClass('active');
-
-        portfolioIsotope.isotope({ filter: $(this).data('filter') });
-    });
-
-    // Search Portfolio
-    document.getElementById('portfolioSearch').addEventListener('input', function () {
-        const searchQuery = this.value.toLowerCase();
-        const projects = document.querySelectorAll('.portfolio-container .portfolio-item');
-
-        projects.forEach(project => {
-            const projectName = project.querySelector('.portfolio-text p').textContent.toLowerCase();
-            if (projectName.includes(searchQuery)) {
-                project.style.display = 'block';  // Show matching projects
-            } else {
-                project.style.display = 'none';   // Hide non-matching projects
-            }
-        });
-    });
-
-    // Experience isotope and filter
-    var experienceIsotope = $('.timeline').isotope({
-        itemSelector: '.experience-container',
-        layoutMode: 'vertical',
-        getSortData: {
-            category: '[class]'
-        }
-    });
-
-    $('#experience-flters li').on('click', function () {
-        $("#experience-flters li").removeClass('active');
-        $(this).addClass('active');
-
-        var filterValue = $(this).attr('data-filter');
-        experienceIsotope.isotope({ filter: filterValue });
-    });
-
-    document.addEventListener("DOMContentLoaded", () => {
-        // Fetch the JSON data
-        fetch("data.json") // Adjust the path if needed
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
-                return response.json();
-            })
-            .then((data) => {
-                populateSkills(data.skills);
-            })
-            .catch((error) => console.error("Error loading skills data:", error));
-    });
-
-    function populateSkills(skills) {
-        console.log("Populating skills...");
-        const skillsContainer = document.getElementById("skills-container");
-
-        if (!skillsContainer) {
-            console.error("Skills container not found!");
-            return;
-        }
-
-        let skillCount = 0; // Initialize a counter to track the number of skills added
-
-        skills.forEach((skill) => {
-            console.log("Adding skill:", skill.name);
-            const skillDiv = document.createElement("div");
-            skillDiv.className = `col-lg-3 col-md-6 skill-container ${skill.type}`;
-            skillDiv.style.marginBottom = "30px"; // Add space below each skill container
-            skillDiv.innerHTML = `
-            <div class="skill-item" style="padding: 10px;">
-                ${skill.icon.startsWith("fa") || skill.icon.startsWith("fab") || skill.icon.startsWith("fas")
-                    ? `<i class="${skill.icon}"></i>`
-                    : `<img src="${skill.icon}" alt="${skill.name} Logo">`
-                }
-                <p class="skill-name">${skill.name}</p>
-            </div>
+            <p class="skill-name">${skill.name}</p>
+        </div>
         `;
-            skillsContainer.appendChild(skillDiv);
+      skillsContainer.appendChild(skillDiv);
+    });
 
-            skillCount++; // Increment the counter
-
-        });
-
-        skillsContainer.style.marginBottom = ((skillCount / 6) * 200).toString() + "px";
-    }
-
-
-
+    // Reinitialize Isotope after populating skills
+    $(".skills-container").isotope("reloadItems").isotope({
+      itemSelector: ".skill-container",
+      layoutMode: "fitRows",
+    });
+  }
 })(jQuery);
--- js/navbar.js
+++ js/navbar.js
@@ -1,54 +1,56 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch('navbar.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('navbar').innerHTML = data;
-        })
-        .catch(error => console.error('Error loading navbar:', error));
+  fetch("navbar.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("navbar").innerHTML = data;
+
+      // Highlight the active navbar link
+      const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+      navLinks.forEach((link) => {
+        const linkHref = link.getAttribute("href").replace("#", ""); // Remove hash for comparison
+        if (linkHref === currentPage) {
+          link.classList.add("active");
+        } else {
+          link.classList.remove("active");
+        }
+      });
+    })
+    .catch((error) => console.error("Error loading navbar:", error));
 });
 
 // Sticky Navbar
 $(window).scroll(function () {
-    if ($(this).scrollTop() > 0) {
-        $('.navbar').addClass('nav-sticky');
-        $('.cv-btn').addClass('nav-sticky');
-    } else {
-        $('.navbar').removeClass('nav-sticky');
-        $('.cv-btn').removeClass('nav-sticky');
-    }
+  if ($(this).scrollTop() > 0) {
+    $(".navbar").addClass("nav-sticky");
+    $(".cv-btn").addClass("nav-sticky");
+  } else {
+    $(".navbar").removeClass("nav-sticky");
+    $(".cv-btn").removeClass("nav-sticky");
+  }
 });
 
 // Smooth scrolling on the navbar links
-$(".navbar-nav a").on('click', function (event) {
-    if (this.hash !== "") {
-        event.preventDefault();
+$(".navbar-nav a").on("click", function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
 
-        $('html, body').animate({
-            scrollTop: $(this.hash).offset().top - 45
-        }, 1500, 'easeInOutExpo');
+    $("html, body").animate(
+      {
+        scrollTop: $(this.hash).offset().top - 45,
+      },
+      1500,
+      "easeInOutExpo"
+    );
 
-        if ($(this).parents('.navbar-nav').length) {
-            $('.navbar-nav .active').removeClass('active');
-            $(this).closest('a').addClass('active');
-        }
+    if ($(this).parents(".navbar-nav").length) {
+      $(".navbar-nav .active").removeClass("active");
+      $(this).closest("a").addClass("active");
     }
+  }
 });
 
-var btn = document.querySelector(".cv-btn");
-
-if (btn) {
-    btn.addEventListener("mouseover", function () {
-        this.textContent = "Download";
-    });
-    btn.addEventListener("mouseout", function () {
-        this.textContent = "CV";
-    });
-} else {
-    console.warn("Button with class .cv-btn not found!");
-}
-
-
+// Open PDF function
 function openPDF() {
-    const url = './Brooke-Cronin-cv.pdf';
-    window.open(url, '_blank');
+  const url = "./Brooke-Cronin-cv.pdf";
+  window.open(url, "_blank");
 }
